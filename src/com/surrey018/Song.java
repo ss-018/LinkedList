f732b61cd8c5cd884bfd353ace02e7ef748ecf1b
Sarjodh Dhillon <sarjodh@healmyhero.com>
2018-08-16 19:57:18 -0700

    First Commit

---
+++ src/com/surrey018/Song.java
@@ -0,0 +1,24 @@
+package com.surrey018;
+
+/**
+ * Author: Sarjodh Dhillon
+ * Date: 05/02/18
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
